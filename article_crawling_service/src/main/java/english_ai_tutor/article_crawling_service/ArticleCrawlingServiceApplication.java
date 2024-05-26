c673279d1f552e7b1163578bef52227b241fcc20
hyunwon <khw73850@gmail.com>
2024-05-26 15:10:27 +0900

    init: crwaling service 생성

---
+++ article_crawling_service/src/main/java/english_ai_tutor/article_crawling_service/ArticleCrawlingServiceApplication.java
@@ -0,0 +1,13 @@
+package english_ai_tutor.article_crawling_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleCrawlingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleCrawlingServiceApplication.class, args);
+	}
+
+}
