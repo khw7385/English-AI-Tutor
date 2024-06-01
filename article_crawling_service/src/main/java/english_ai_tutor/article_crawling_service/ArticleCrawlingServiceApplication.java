5bc1913318535c2ff86970a8bebe2e975cdf62d9
hyunwon <khw73850@gmail.com>
2024-06-01 15:14:08 +0900

    feat: 스케줄링 적용

--- article_crawling_service/src/main/java/english_ai_tutor/article_crawling_service/ArticleCrawlingServiceApplication.java
+++ article_crawling_service/src/main/java/english_ai_tutor/article_crawling_service/ArticleCrawlingServiceApplication.java
@@ -2,7 +2,9 @@ package english_ai_tutor.article_crawling_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ArticleCrawlingServiceApplication {
 
